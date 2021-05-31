d151e2815b6dac479be927f32d4074d25db5d357
Ash515 <aaashwin515@gmail.com>
2021-05-31 22:15:00 +0530

    added array

---
+++ Arrays/Difficulty-Easy/Removeelements.py
@@ -0,0 +1,16 @@
+#27. Remove Element
+'''
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length
+'''
+class Solution:
+    def removeElement(self, nums,val):
+        i=0
+        for n in nums:
+            if n!=val:
+                nums[i] = n
+                i+=1
+        return i
+       
+            
